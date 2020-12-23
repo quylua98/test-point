f228597491fe0ccf9063da05c4ca2b7004cf9041
Nguyễn Duy Quý <quynd@rabiloo.com.vn>
2020-12-23 16:31:59 +0700

    update method

--- src/main/java/com/quylua98/Application.java
+++ src/main/java/com/quylua98/Application.java
@@ -4,20 +4,20 @@ import com.quylua98.dto.ResponseData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @SpringBootApplication
-@RestController
+@Controller
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@PostMapping("/api/altema/pointsget_api")
-	public ResponseEntity<?> getPoint(@RequestParam String suid) {
+	public ResponseEntity<?> getPoint(@RequestBody String suid) {
 		return ResponseEntity.ok(new ResponseData(suid, 1000, 200, "success"));
 	}
 
